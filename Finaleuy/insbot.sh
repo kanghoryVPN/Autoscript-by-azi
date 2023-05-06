4fb8c82dc53477e266e7d2e4d80dd5972cd85752
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-06 20:50:14 +0700

    Update insbot.sh

--- Finaleuy/insbot.sh
+++ Finaleuy/insbot.sh
@@ -3,3 +3,5 @@
 wget -q -O /usr/bin/bot "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Finaleuy/ssh-telegram.sh"
 #izin bot
 chmod +x /usr/bin/bot
+# install server bot
+wget -O /etc/systemd/system/
