14ff3f61a08443a7cf8a2ffbcac4e701eca74c6d
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-10 00:41:59 +0700

    Update chatid.sh

--- Finaleuy/chatid.sh
+++ Finaleuy/chatid.sh
@@ -1,3 +1,12 @@
+
+
+clear
+
+echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m         °BOT NOTIF MULTILOGIN°          \E[0m"
+echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+
+
 read -rp "Masukkan chat id mu: " -e sub
 chatid=${sub}
 chat_id=${chatid}
