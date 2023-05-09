8ef77b00a60e23661d1caa3501c9ad78c9717a4d
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-10 00:46:32 +0700

    Update chatid.sh

--- Finaleuy/chatid.sh
+++ Finaleuy/chatid.sh
@@ -1,4 +1,13 @@
 
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
 
 clear
 
@@ -11,4 +20,10 @@ read -rp "Masukkan chat id mu: " -e sub
 chatid=${sub}
 chat_id=${chatid}
 echo $chat_id > /root/id
+
+echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+
+echo -e "${LIGHT}                     SCRIPT CREDIT BY CYBERVPN"
+
+echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 
