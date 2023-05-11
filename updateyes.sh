a253a37825a11d1ea42efab8445cb93fc317f88f
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-11 23:53:26 +0700

    Update updateyes.sh

--- updateyes.sh
+++ updateyes.sh
@@ -10,12 +10,12 @@ rm -f /usr/bin/usernew
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu.sh"
 wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Themes/setting.sh"
 wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu/usernew.sh"
-
+wget -q -O /usr/bin/autokill "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/limit/autokill.sh"
 #izin
 chmod 777 /usr/bin/usernew
 chmod 777 /usr/bin/menu
 chmod 777 /usr/bin/setting
-
+chmod 777 /usr/bin/autokill
 
 rm -rf updateyes.sh
 menu
