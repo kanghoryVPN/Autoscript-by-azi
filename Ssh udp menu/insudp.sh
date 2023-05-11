108a17fdffb81de44013d9e3036395bb87e93968
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-11 20:22:16 +0700

    Update insudp.sh

--- Ssh udp menu/insudp.sh
+++ Ssh udp menu/insudp.sh
@@ -1,4 +1,21 @@
 clear
+
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+export BOLD="\e[1m"
+
 figlet -f 3d "installing ssh udp" | lolcat
 echo "UDP SERVICE BY CYBERVPN" | lolcat
 echo "proses instalasi sedang berjalan..!!" | lolcat
