0db44534a7a23b55faa262d1a094e3334597df37
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-11 21:50:39 +0700

    Update updateyes.sh

--- updateyes.sh
+++ updateyes.sh
@@ -7,17 +7,16 @@ rm -f /usr/bin/menu
 rm -f /usr/bin/usernew
 rm -f /usr/bin/autokill
 #download
-wget -q -O /usr/bin/autokill "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/limit/autokill.sh"
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu.sh"
 wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Themes/setting.sh"
 wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu/usernew.sh"
-wget -q -O insudp.sh https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Ssh%20udp%20menu/insudp.sh && chmod +x insudp.sh && ./insudp.sh
 
 #izin
 chmod 777 /usr/bin/usernew
 chmod 777 /usr/bin/menu
 chmod 777 /usr/bin/setting
 
+wget -q -O insudp.sh https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Ssh%20udp%20menu/insudp.sh && chmod +x insudp.sh && ./insudp.sh
 
 rm -rf updateyes.sh
 menu
