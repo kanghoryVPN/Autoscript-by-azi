49527d572fc2e6bf4907b452371b20538507f9e1
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-12 16:37:30 +0700

    Create info.sh

---
+++ limit/info.sh
@@ -0,0 +1,6 @@
+figlet -f 3d "CyberVPN Tunneling" | lolcat
+cat /root/log-install.txt
+echo "-----------------------------------------------------"
+echo "                Credit Script By Aji permana" | lolcat
+echo "                   Author Script CyberVPN" | lolcat
+echo "-----------------------------------------------------"
