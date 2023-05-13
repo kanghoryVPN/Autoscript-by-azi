0aa63612bd9a76238ef23027c31a2de7ff6211dd
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-13 20:06:02 +0700

    Create tes.sh

---
+++ Finaleuy/tes.sh
@@ -0,0 +1,13 @@
+domain=$(cat /etc/xray/domain)
+LocalVersion=$(cat /root/versi)
+IPVPS=$(curl -s ipinfo.io/ip )
+ISPVPS=$( curl -s ipinfo.io/org )
+token=5922026926:AAE5t2CXnOOT57zWdua2wfHKKG9URGEQdP0
+chatid=1106186898
+ttoday="$(vnstat | grep today | awk '{print $8" "substr ($9, 1, 3)}' | head -1)"
+tmon="$(vnstat -m | grep `date +%G-%m` | awk '{print $8" "substr ($9, 1 ,3)}' | head -1)"
+DATE_EXEC="$(date "+%d %b %Y %H:%M")"
+CITY=$(cat $TMPFILE | jq '.city' | sed 's/"//g')
+REGION=$(cat $TMPFILE | jq '.region' | sed 's/"//g')
+COUNTRY=$(cat $TMPFILE | jq '.country' | sed 's/"//g')
+curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id="$chatid" -d text="$IPVPS domain $domain telah install XrayCol pada $DATE_EXEC di $CITY, $REGION via $ORG" > /dev/null 2>&1
