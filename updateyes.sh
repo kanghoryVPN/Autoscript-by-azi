a7e8136fc532899cf19b83497f07ff3ffe80c2e4
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-05-17 10:21:29 +0700

    Update updateyes.sh

--- updateyes.sh
+++ updateyes.sh
@@ -5,18 +5,19 @@ sleep 2
 rm -f /usr/bin/setting
 rm -f /usr/bin/menu
 rm -f /usr/bin/usernew
-
+rm -f /usr/bin/menu-ssh
 #download
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu.sh"
 wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Themes/setting.sh"
 wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu/usernew.sh"
 wget -q -O /usr/bin/autokill "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/limit/autokill.sh"
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu-ssh.sh"
 echo "*/3 * * * * root bot" >> /etc/crontab
 #izin
 chmod 777 /usr/bin/usernew
 chmod 777 /usr/bin/menu
 chmod 777 /usr/bin/setting
 chmod 777 /usr/bin/autokill
-
+chmod 777 /usr/bin/menu-ssh
 rm -rf updateyes.sh
 menu
